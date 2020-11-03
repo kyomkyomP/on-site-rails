39299c5bc06654b66d99fc87d78c87d2cd1e9915
kyomkyomP <kyomkyomp@gmail.com>
2020-11-03 17:49:06 +0900

    テスト実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params.merge(user_id: current_user.id))
     
-    if @task.save!
+    if @task.save
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
