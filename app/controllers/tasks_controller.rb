ec71b3fc660fe992eb721e980c844e2ea92e38da
kyomkyomP <kyomkyomp@gmail.com>
2020-11-06 03:54:18 +0900

    タスクに画像ファイルを添付できるように変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
 
   def update
     @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+    redirect_to @task, notice: "タスク「#{@task.name}」を更新しました。"
   end
 
   def destroy
@@ -50,7 +50,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description) 
+    params.require(:task).permit(:name, :description, :image) 
   end
 
   def set_task
